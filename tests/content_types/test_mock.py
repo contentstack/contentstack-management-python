5544e9dcc053e5293754206a44f9e68b94da0b42
harshithad0703 <harshitha.d@contentstack.com>
2023-06-23 16:10:50 +0530

    test: removed unwanted test files

--- tests/content_types/test_mock.py
+++
@@ -1,7 +0,0 @@
-
-def read_file(self, file_name):
-    file_path= f"tests/resources/mockContentType/{file_name}"
-    infile = open(file_path, 'r')
-    data = infile.read()
-    infile.close()
-    return data
